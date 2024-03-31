c90d21fbd5d791c5e0b4204acb1dfa4e34a1f9e2
Mhamad6000 <mhamadothman6000@gmail.com>
2024-03-31 12:44:19 -0700

    added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,25 +4,25 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import Link from "next/link";
 import { RiInboxArchiveLine, RiInboxArchiveFill } from "react-icons/ri";
-export async function GetCategories() {
-  const options = {
-    method: "GET",
-    url: "https://clickai-api.kurdmake.com/category/",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + Cookies.get("auth_token"),
-    },
-  };
-  try {
-    const { data } = await axios.request(options);
-    return data;
-  } catch (error) {
-    return [];
-  }
-}
+// export async function GetCategories() {
+//   const options = {
+//     method: "GET",
+//     url: "https://clickai-api.kurdmake.com/category/",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: "Bearer " + Cookies.get("auth_token"),
+//     },
+//   };
+//   try {
+//     const { data } = await axios.request(options);
+//     return data;
+//   } catch (error) {
+//     return [];
+//   }
+// }
 
 export default async function Home() {
-  const categories = await GetCategories();
+  // const categories = await GetCategories();
 
   return (
     <main className="flex min-h-screen flex-col py-10 px-4 md:px-8 lg:px-12 bg-primary  text-gray-100 max-w-7xl mx-auto">
--- src/app/sentences/not_completed/page.jsx
+++ src/app/sentences/not_completed/page.jsx
@@ -4,24 +4,24 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 import Sentences from "./components/Sentences";
-export async function GetSentences() {
-  const options = {
-    method: "GET",
-    url: "https://clickai-api.kurdmake.com/sentence",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + Cookies.get("auth_token"),
-    },
-  };
-  try {
-    const { data } = await axios.request(options);
-    console.log(data, "data");
-    return data;
-  } catch (error) {
-    console.log(error, "error");
-    return [];
-  }
-}
+// export async function GetSentences() {
+//   const options = {
+//     method: "GET",
+//     url: "https://clickai-api.kurdmake.com/sentence",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: "Bearer " + Cookies.get("auth_token"),
+//     },
+//   };
+//   try {
+//     const { data } = await axios.request(options);
+//     console.log(data, "data");
+//     return data;
+//   } catch (error) {
+//     console.log(error, "error");
+//     return [];
+//   }
+// }
 export default async function Home({ params: { condition } }) {
   // const sentencesData = await GetSentences();
 
